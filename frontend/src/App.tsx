47626e7d4616fbf36e20f9409f3122925cb2f551
Veysel AKSIN <veysel.aksin@trendbox.io>
2024-05-21 13:34:48 +0300

    Frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+export default function App() {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+}
