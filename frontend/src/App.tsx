27c25cf7347c73db45703877f73cde239cbe53c5
Veysel Aksin <32255510+veyselaksin@users.noreply.github.com>
2024-05-25 22:36:18 +0300

    chore: Update npm dependencies and add Tailwind CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,21 @@ import {
   UserButton,
   useUser,
 } from "@clerk/clerk-react";
+import { useEffect } from "react";
 
 export default function App() {
   const authDetails = useAuth();
-  console.log(authDetails);
+  console.log("TOKEN: ", authDetails.getToken().then((token) => console.log(token)));
 
   const user = useUser();
   console.log(user);
+
+  // user list
+  useEffect(() => {
+    fetch("http://localhost:3000")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <div className="h-screen flex items-center justify-center flex-col gap-5">
